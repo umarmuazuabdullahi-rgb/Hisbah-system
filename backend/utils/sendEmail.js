f381c535c4df6d9380df3b406afc79b82b45bced
umarmuazuabdullahi-rgb <umarmuazuabdullahi@gmail.com>
2026-06-01 02:17:52 +0100

    Delete backend directory

--- backend/utils/sendEmail.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require("nodemailer");
-
-module.exports = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: { user: process.env.EMAIL, pass: process.env.EMAIL_PASS }
-  });
-  await transporter.sendMail({ from: "Hisbah", to, subject, text });
-};
